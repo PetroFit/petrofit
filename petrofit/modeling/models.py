341120a76f914e030575297a007c5814ac55acfa
Robel Geda <robelgeda@yahoo.com>
2023-02-21 20:13:22 -0500

    Remove example from astropy in GenSersic2D

--- petrofit/modeling/models.py
+++ petrofit/modeling/models.py
@@ -568,33 +568,6 @@ class GenSersic2D(models.Sersic2D):
 
         \Gamma(2n) = 2\gamma (2n,b_n)
 
-    Examples
-    --------
-    .. plot::
-        :include-source:
-
-        import numpy as np
-        from astropy.modeling.models import Sersic2D
-        import matplotlib.pyplot as plt
-
-        x,y = np.meshgrid(np.arange(100), np.arange(100))
-
-        mod = Sersic2D(amplitude = 1, r_eff = 25, n=4, x_0=50, y_0=50,
-                       ellip=.5, theta=-1)
-        img = mod(x, y)
-        log_img = np.log10(img)
-
-
-        plt.figure()
-        plt.imshow(log_img, origin='lower', interpolation='nearest',
-                   vmin=-1, vmax=2)
-        plt.xlabel('x')
-        plt.ylabel('y')
-        cbar = plt.colorbar()
-        cbar.set_label('Log Brightness', rotation=270, labelpad=25)
-        cbar.set_ticks([-1, 0, 1, 2], update_ticks=True)
-        plt.show()
-
     References
     ----------
     .. [1] http://ned.ipac.caltech.edu/level5/March05/Graham/Graham2.html
